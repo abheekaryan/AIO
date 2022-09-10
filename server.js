921f3ba18e9d47ce1c10e6862d1a2529bd707070
abheekaryan <i.m.abheek@gmail.com>
2022-09-10 12:58:41 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const  app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.get('/',(req, res)=>{
+    res.send("hello world")
+})
+
+io.on('connection', (socket) => {
+    console.log('Client connected'+socket.id)
+    socket.on('CHAT',(data)=>{
+        console.log('========CHAT msg=======')
+        console.log(data)
+        socket.emit('CHAT',data)
+    })
+    socket.on('disconnect',()=>{
+        console.log('Client disconnected'+socket.id)
+    })
+})
+
+server.listen(3200,function(){
+    console.log('Listening on port 3200')
+})
+
+
