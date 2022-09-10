ff5de8e7b2012c30b8c980c532c93280ce3c5aaf
abheekaryan <i.m.abheek@gmail.com>
2022-09-10 16:58:28 +0530

    5

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const  app = require('express')()
 const http = require('https')
-const server = http.createServer(app)
+const index = http.createServer(app)
 const { Server} = require('socket.io')
-const io = new Server(server)
+const io = new Server(index)
 
 app.get('/',(req, res)=>{
     res.send("<h1>hello world<h1>")
@@ -11,7 +11,7 @@ app.get('/',(req, res)=>{
 io.on('connection', (socket) => {
     console.log('Client connected'+socket.id)
     socket.on('CHAT',(data)=>{
-        console.log('========CHAT msg=======')
+        console.log('========CHAT msg======')
         console.log(data)
         socket.emit('CHAT',data)
     })
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
     })
 })
 
-server.listen(3200,function(){
+index.listen(3200,function(){
     console.log('Listening on port 3200')
 })
 
